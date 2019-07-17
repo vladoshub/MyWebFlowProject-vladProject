f6f4e5b5ddea674c5a69d2d6f450df268b51f26f
vyarovenko <vladosyarovenko@gmail.com>
2019-07-17 13:13:32 +0300

    webflowProject

--- src/main/webapp/js/form/myScripts.js
+++ src/main/webapp/js/form/myScripts.js
@@ -5,17 +5,18 @@ var check = 0;
 
 function editKeyOrSingleWord(id, op, newword) {
     newword = newword + "";
-    if (newword == document.getElementById(id).value || newword == "") {
+    if (newword == $("#"+id).val() || newword == "") {
         alert("введите что-нибудь или измененное значение");
     }
     else {
         check = 0;
         if ((op + '') == 'keys') {
-            document.getElementById("keys").value = document.getElementById(id).value;
-            document.getElementById("ID").value = document.getElementById(id).name;
+
+            $("#keys").attr("name",$('#' + id).val());
+            $("#ID").val( $("#"+id).attr("name"));
             $("#forma1").ajaxSubmit({
 
-                url: "${flowExecutionUrl}&_eventId_editKey&ajaxSource=true",
+                url: $("#flow").val()+"&_eventId_editKey&ajaxSource=true",
                 success: function (html) {
                     $("#voc").html($(html).filter("#voc")),
                         $("#answer").html($(html).filter("#answer"));
@@ -26,11 +27,11 @@ function editKeyOrSingleWord(id, op, newword) {
             });
         }
         else {
-            document.getElementById("words").value = document.getElementById(id).value;
-            document.getElementById("ID").value = document.getElementById(id).name;
+            $("#words").val( $("#"+id).val());
+            $("#ID").val( $("#"+id).attr("name"));
             $("#forma1").ajaxSubmit({
 
-                url: "${flowExecutionUrl}&_eventId_editWord&ajaxSource=true",
+                url: $("#flow").val()+"&_eventId_editWord&ajaxSource=true",
                 success: function (html) {
                     $("#voc").html($(html).filter("#voc")),
                         $("#answer").html($(html).filter("#answer"));
@@ -71,7 +72,7 @@ function deleteKeyOrSingleWord(id, op) {
         $("#ID").val($("#"+id).attr('name'));
         $("#forma1").ajaxSubmit({
 
-            url: "${flowExecutionUrl}&_eventId_deletedWord&ajaxSource=true",
+            url: $("#flow").val()+"&_eventId_deletedWord&ajaxSource=true",
             success: function (html) {
                 $("#voc").html($(html).filter("#voc")),
                     $("#answer").html($(html).filter("#answer"));
@@ -87,7 +88,7 @@ function deleteKeyOrSingleWord(id, op) {
 function addAllWord(id, idKey) {
     check = 0;
     var words = "";
-    var x = document.getElementById(id).getElementsByTagName('input');
+    var x = $('#'+id+' input');
     for (var i = 0; i < x.length; i++) {
         if (x[i].value != "") {
             words = words + x[i].value + "#_#";
@@ -95,10 +96,10 @@ function addAllWord(id, idKey) {
     }
     if (words != "") {
         words = words.substring(0, words.length - 3);
-        document.getElementById("words").value = words;
-        document.getElementById("ID").value = idKey;
+        $('#words').val(words);
+        $('#ID').val(idKey);
         $("#forma1").ajaxSubmit({
-            url: "${flowExecutionUrl}&_eventId_editManyWordsInKey&ajaxSource=true",
+            url: $("#flow").val()+"&_eventId_editManyWordsInKey&ajaxSource=true",
             success: function (html) {
                 $("#voc").html($(html).filter("#voc")),
                     $("#answer").html($(html).filter("#answer"));
@@ -126,29 +127,29 @@ function addWord(id, idKey) {
     var a = "rows" + rows;
     rows++;
     var tbody = $('#' + id);
-    var row = document.createElement("TR");
-    var td1 = document.createElement("TD");
-    var td2 = document.createElement("TD");
-    var td3 = document.createElement("TD");
-    var inp = document.createElement("input");
-    var butSave = document.createElement("button");
-    var butDel = document.createElement("button");
-    td1.style = "border-top:0";
-    td2.style = "border-top:0";
-    td3.style = "border-top:0";
-    inp.id = a + "input";
-    inp.name = idKey + '';
-    row.id = a + "R";
-    butSave.title = "Сохранить";
-    butSave.type = "button";
-    butSave.name = "_eventId_addWordsss";
-    butSave.style = "background: url(/images/upload.png);background-size: 38px 35px;";
-    butDel.title = "Удалить";
-    butDel.style = "background: url(/images/deleteKeyOrSingleWord.png);background-size: 38px 35px;";
+    var row = $('<tr></tr>');
+    var td1 = $('<td></td>');
+    var td2 = $('<td></td>');
+    var td3 = $('<td></td>');
+    var inp = $('<input>');
+    var butSave = $('<button></button>');
+    var butDel = $('<button></button>');
+    td1.attr("style","border-top:0");
+    td2.attr("style","border-top:0");
+    td3.attr("style","border-top:0");
+    inp.attr("id",a + "input");
+    inp.attr("name",idKey + '');
+    row.attr("name",a + "R");
+    butSave.attr("title","Сохранить");
+    butSave.attr("type","button");
+    butSave.attr("name","_eventId_addWordsss");
+    butSave.attr("style","background: url(/images/upload.png);background-size: 38px 35px;");
+    butDel.attr("title","Удалить");
+    butDel.attr("style","background: url(/images/del.png);background-size: 38px 35px;");
     $(butDel).on('click',function () {
-        $("#" + row.id).remove();
+        $("#" + row.attr("id")).remove();
         check--;
-        var x = $("#" + id).getElementsByTagName('input');
+        var x = $("#" + id+" input");
         if (check == 1) {
             $('#butAllWord' + idKey).attr("style", "background: url(/images/addAllBW.png);background-size:38px 35px");
             $('#butAllWord' + idKey).attr("disabled", true);
@@ -165,13 +166,13 @@ function addWord(id, idKey) {
         check = 0;
         var id = a + "input";
         var id2 = a + "R";
-        var text = document.getElementById(id).value;
-        var text2 = document.getElementById(id).name;
-        document.getElementById("words").value = text;
-        document.getElementById("ID").value = text2;
+        var text = $("#"+id).val();
+        var text2 = $("#"+id).attr("name");
+        $("#words").val(text);
+        $("#ID").val(text2);
         $("#forma1").ajaxSubmit({
 
-            url: "${flowExecutionUrl}&_eventId_addWordsss&ajaxSource=true",
+            url: $("#flow").val()+"&_eventId_addWordsss&ajaxSource=true",
             success: function (html) {
                 $("#voc").html($(html).filter("#voc")),
                     $("#answer").html($(html).filter("#answer"));
@@ -182,99 +183,95 @@ function addWord(id, idKey) {
         });
     });
 
-    td2.appendChild(inp);
-    td3.appendChild(butDel);
-    td3.appendChild(butSave);
-    row.appendChild(td1);
-    row.appendChild(td2);
-    row.appendChild(td3);
-    row.style = "background: deepskyblue;";
-    tbody.appendChild(row);
+    td2.append(inp);
+    td3.append(butDel);
+    td3.append(butSave);
+    row.append(td1);
+    row.append(td2);
+    row.append(td3);
+    row.attr("style","background: deepskyblue;");
+    tbody.append(row);
 }
 
 function addKey(id) {
     var a = "rows1" + rows1;
     rows1++;
-    var table = document.getElementById(id);
-    var row = document.createElement("TR");
-    var td1 = document.createElement("TD");
-    var td2 = document.createElement("TD");
-    var td3 = document.createElement("TD");
-    var inp = document.createElement("input");
-    var inp2 = document.createElement("input");
-    var butSave = document.createElement("button");
-    var butDel = document.createElement("button");
-    var addWord = document.createElement("button");
-    inp.id = a + "inputWord";
-    inp2.id = a + "inputKey";
-    row.id = a + "R";
-    addWord.title = "Добавить слово";
-    addWord.type = "button";
-    addWord.style = "background: url(/images/addKey.png);background-size: 38px 35px;  margin-left:25px;";
-    butSave.title = "Сохранить";
-    butSave.type = "button";
-    butSave.style = "background: url(/images/upload.png);background-size: 38px 35px; float:right; margin-left:10px;";
-    butDel.title = "Удалить";
-    butDel.style = "background: url(/images/deleteKeyOrSingleWord.png);background-size: 38px 35px;";
-    butDel.onclick = function () {
+    var table = $('#'+id);
+    var row = $('<tr></tr>');
+    var td1 = $('<td></td>');
+    var td2 = $('<td></td>');
+    var td3 = $('<td></td>');
+    var inp = $('<input>');
+    var inp2 = $('<input>');
+    var butSave = $('<button></button>');
+    var butDel = $('<button></button>');
+    var addWord = $('<button></button>');
+    inp.attr("id",a + "inputWord");
+    inp2.attr("id",a + "inputKey");
+    row.attr("id",a + "R");
+    addWord.attr("title","Добавить слово");
+    addWord.attr("type","button");
+    addWord.attr("style","background: url(/images/addKey.png);background-size: 38px 35px;  margin-left:25px;");
+    butSave.attr("title","Сохранить");
+    butSave.attr("type","button");
+    butSave.attr("style","background: url(/images/upload.png);background-size: 38px 35px; float:right; margin-left:10px;");
+    butDel.attr("title","Удалить");
+    butDel.attr("style","background: url(/images/del.png);background-size: 38px 35px;");
+    butDel.on('click',function () {
         var id = a + "R";
-        var x = document.getElementsByName("rowWord");
-        while (x[0]) x[0].remove();//при обычном for длина массива будет динамически менятся при удалении элемента
-        document.getElementById(id).remove();
+        $("[name='rowWord[]']").remove();
+        $("#" + id).remove();
         $('#ButAdd').attr('disabled', false);
         $('#ButAdd').attr('style', "background: url(/images/addKey.png);background-size: 38px 35px;float:right;margin-left:10px");
-    }
-    addWord.onclick = function () {
-        var butDel = document.createElement("button");
+    });
+    addWord.on('click',function () {
+        var butDel = $('</button>');
         var id = wordInKey + "manyWord";
-        butDel.onclick = function () {
-            document.getElementById(id).remove();
-        }
-        butDel.style = "background: url(/images/deleteKeyOrSingleWord.png);background-size: 38px 35px;";
-        var tdW = document.createElement("TD");
-        var tdWInp = document.createElement("TD");
-        var tdWBut = document.createElement("TD");
-        tdW.style = "border-top:0";
-        tdWInp.style = "border-top:0";
-        tdWBut.style = "border-top:0";
-        var inpW = document.createElement("input");
-        inpW.setAttribute("name", "allInp");
-        tdWBut.appendChild(butDel);
-        var rowW = document.createElement("TR");
-        rowW.setAttribute("name", "rowWord");
-        rowW.setAttribute("id", id);
+        butDel.on('click',function () {
+            $("#" + id).remove();
+        });
+        butDel.attr("style","background: url(/images/del.png);background-size: 38px 35px;");
+        var tdW = $('<td></td>');
+        var tdWInp = $('<td></td>');
+        var tdWBut = $('<td></td>');
+        tdW.attr("style","border-top:0");
+        tdWInp.attr("style","border-top:0");
+        tdWBut.attr("style","border-top:0");
+        var inpW = $('<input>');
+        inpW.attr("name", "allInp");
+        tdWBut.append(butDel);
+        var rowW = $('<tr></tr>');
+        rowW.attr("name", "rowWord");
+        rowW.attr("id", id);
         wordInKey = wordInKey + 1;
-        tdWInp.appendChild(inpW);
-        rowW.appendChild(tdW);
-        rowW.appendChild(tdWInp);
-        rowW.appendChild(tdWBut);
-        rowW.style = "background: LIGHTSEAGREEN";
-        table.appendChild(rowW);
+        tdWInp.append(inpW);
+        rowW.append(tdW);
+        rowW.append(tdWInp);
+        rowW.append(tdWBut);
+        rowW.attr("style","background: LIGHTSEAGREEN");
+        table.append(rowW);
 
 
-    }
+    });
 
-    butSave.onclick = function () {
+    butSave.on('click',function () {
         check = 0;
         var words = a + "inputWord";
         var id2 = a + "R";
         var keys = a + "inputKey";
         var allW = "";
-        var word2 = document.getElementById(words).value;
-        var key2 = document.getElementById(keys).value;
-        var x = document.getElementsByName("allInp");
-        for (var i = 0; i < x.length; i++) {
-            if (x[i].value != "") {
-                allW = allW + x[i].value + "#_#";
-            }
-        }
+        var word2 = $("#"+words).val();
+        var key2 = $("#"+keys).val();
+        jQuery("input[name='allInp']").each(function() {
+            allW = allW + this.value + "#_#";
+        });
         if (allW != "") {
             allW = allW.substring(0, allW.length - 3);
-            butSave.name = "_addManyWordsAndKey";
-            document.getElementById("words").value = word2 + "#_#" + allW;
-            document.getElementById("keys").value = key2;
+            butSave.attr("name","_addManyWordsAndKey");
+            $("#words").val(word2 + "#_#" + allW);
+            $("#keys").val(key2);
             $("#forma1").ajaxSubmit({
-                url: "${flowExecutionUrl}&_eventId_addManyWordsAndKey&ajaxSource=true",
+                url: $("#flow").val()+"&_eventId_addManyWordsAndKey&ajaxSource=true",
                 success: function (html) {
                     $("#voc").html($(html).filter("#voc")),
                         $("#answer").html($(html).filter("#answer"));
@@ -294,11 +291,11 @@ function addKey(id) {
                     alert("введите ключ ")
                 }
                 else {
-                    butSave.name = "_eventId_addWordss";
-                    document.getElementById("words").value = word2;
-                    document.getElementById("keys").value = key2;
+                    butSave.attr("name","_eventId_addWordss");
+                    $("#words").attr("value",word2);
+                    $("#keys").attr("value",key2);
                     $("#forma1").ajaxSubmit({
-                        url: "${flowExecutionUrl}&_eventId_addWordss&ajaxSource=true",
+                        url: $("#flow").val()+"&_eventId_addWordss&ajaxSource=true",
                         success: function (html) {
                             $("#voc").html($(html).filter("#voc")),
                                 $("#answer").html($(html).filter("#answer"));
@@ -311,39 +308,38 @@ function addKey(id) {
 
             }
         }
-    }
+    });
 
-    td1.appendChild(inp2);
-    td2.appendChild(inp);
-    td3.appendChild(butDel);
-    td3.appendChild(butSave);
-    td3.appendChild(addWord);
-    row.appendChild(td1);
-    row.appendChild(td2);
-    row.appendChild(td3);
-    row.style = "background: lightblue;";
-    table.appendChild(row);
-    table.appendChild(row);
-    $('#ButAdd').attr('disabled', true);
+    td1.append(inp2);
+    td2.append(inp);
+    td3.append(butDel);
+    td3.append(butSave);
+    td3.append(addWord);
+    row.append(td1);
+    row.append(td2);
+    row.append(td3);
+    row.attr("style","background: lightblue;");
+    table.append(row);
+    $('#ButAdd').prop('disabled', true);
     $('#ButAdd').attr('style', "background: url(/images/addKeyBW.png);background-size: 38px 35px;float:right;margin-left:10px");
 }
 
 
 function editWords(id, id2, newword) {
     newword = newword + "";
-    if (newword == document.getElementById(id).value || newword == "") {
+    if (newword == $("#"+id).val() || newword == "") {
         alert("введите что-нибудь или измененное значение");
     }
     else {
         check = 0;
-        document.getElementById("words").value = document.getElementById(id).value;
-        document.getElementById("ID").value = document.getElementById(id).name;
+        $("#words").val($("#"+id).val());
+        $("#ID").val($("#"+id).attr());
         var ids = "";
         ids = ids + id2;
-        document.getElementById("kID").value = ids;
+        $("#kID").val(ids);
         $("#forma1").ajaxSubmit({
 
-            url: "${flowExecutionUrl}&_eventId_editWord&ajaxSource=true",
+            url: $("#flow").val()+"&_eventId_editWord&ajaxSource=true",
             success: function (html) {
                 $("#voc").html($(html).filter("#voc")),
                     $("#answer").html($(html).filter("#answer"));
@@ -356,14 +352,14 @@ function editWords(id, id2, newword) {
 }
 
 function deleteWords(id, id2) {
-    document.getElementById("ID").value = document.getElementById(id).name;
+    $("#ID").val($("#"+id).attr("name"));
     var ids = "";
     ids = ids + id2;
-    document.getElementById("kID").value = ids;
+    $("#kID").val(ids);
     check = 0;
     $("#forma1").ajaxSubmit({
 
-        url: "${flowExecutionUrl}&_eventId_deletedWord&ajaxSource=true",
+        url: $("#flow").val()+"&_eventId_deletedWord&ajaxSource=true",
         success: function (html) {
             $("#voc").html($(html).filter("#voc")),
                 $("#answer").html($(html).filter("#answer"));
